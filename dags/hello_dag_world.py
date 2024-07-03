e0119b29d8c902a8b32c9fae82c4ab7f9690cdc9
Nemiop <timchenkoruslan97@gmail.com>
2024-07-03 23:43:46 +0200

    another DAG form

--- dags/hello_dag_world.py
+++ dags/hello_dag_world.py
@@ -9,19 +9,19 @@ default_args = {
     'owner' : 'redbeast',
 }
 
-dag = DAG(
+with DAG(
     dag_id = 'hello_world',
     description = "My first DAG 'Hello World!'",
     default_args = default_args,
     start_date = days_ago(1),
     schedule_interval = '@daily',
     tags = ["beginner", "bash", "hw"]
-)
+) as dag:
 
-task = BashOperator(
-    task_id = "hello_world_task",
-    bash_command = "echo hello world again!",
-    dag = dag
-)
+    task = BashOperator(
+        task_id = "hello_world_task",
+        bash_command = "echo With hello world again!",
+        dag = dag
+    )
 
 task
